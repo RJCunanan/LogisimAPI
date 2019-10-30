37ec8493ffc050fa5bd50ce9892edc7c6f73af97
RJCunanan <rjlugtuc@gmail.com>
2019-10-30 08:20:29 -0700

    Fixed reuploading the app file to repository

---
+++ app/src/main/java/com/example/logisim/LAMP.java
@@ -0,0 +1,52 @@
+/*
+ Team API:
+ Jaime Rivera
+ RJ Cunanan
+ Theodora Fernandez
+ Yong Yang
+
+ Group Project: LogiSim
+ Class: CSC 131
+ Date: 10-30-19
+ */
+
+package com.example.logisim;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+public class LAMP extends Cell {
+
+    // Unique value assigned to Lamps
+    private int gateNum = 5;
+
+    // Used for drawing
+    private Bitmap bitmap;
+    private RectF mRect;
+
+    public LAMP(Cell myCell) {super(myCell);}
+
+    // Sets the bitmap to the Lamp image and prints within the cell's space
+    void drawCell(Paint paint, Canvas canvas, Context context) {
+        if(state)
+            bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.lamp1);
+        else
+            bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.lamp0);
+
+        mRect = new RectF(cellX, cellY, cellWidth, cellHeight);
+        canvas.drawBitmap(bitmap,null,mRect, paint);
+    }
+
+    int getGateNum() {return gateNum;}
+
+    void evalLamp() {
+        state = eval();
+    }
+
+    // Passes its connected cell's state
+    boolean eval() {return a.eval();}
+}
