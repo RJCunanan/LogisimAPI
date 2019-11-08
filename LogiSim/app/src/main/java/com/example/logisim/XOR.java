5ea8f34023d606838143352b29abed8bf1492978
Jaime Rivera <jrrivera0612@gmail.com>
2019-11-07 22:48:09 -0800

    Added constructor and functions that have yet to have logic and pictures designed for XOR

--- LogiSim/app/src/main/java/com/example/logisim/XOR.java
+++ LogiSim/app/src/main/java/com/example/logisim/XOR.java
@@ -1,6 +1,36 @@
 package com.example.logisim;
 
-import Cell;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 
 public class XOR extends Cell {
+    // Unique value assigned to OR gates
+    private int gateNum = 3;
+
+    // Used for drawing
+    private Bitmap bitmap;
+    private RectF mRect;
+
+
+    // converts previous Cell to OR Cell
+    public XOR(Cell myCell) {super(myCell);}
+
+
+    // Sets the bitmap to the OR gate image and prints within the cell's space
+    void drawCell(Paint paint, Canvas canvas, Context context) {
+//        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.or);
+//        mRect = new RectF(cellX, cellY, cellWidth, cellHeight);
+//        canvas.drawBitmap(bitmap,null,mRect,paint);
+    }
+
+
+    int getGateNum() {return gateNum;}
+
+
+    // Either A or B, but not both
+    boolean eval() { return false; }
 }
