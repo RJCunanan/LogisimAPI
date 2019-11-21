391f805d3a10230bbc1eaea99f51e1a3ce225a75
ylygplay@gmail.com <ylygplay@gmail.com>
2019-11-20 20:43:40 -0800

    added and and nand options to statements

--- LogiSim/app/src/main/java/com/example/logisim/Undo.java
+++ LogiSim/app/src/main/java/com/example/logisim/Undo.java
@@ -15,27 +15,11 @@ public class Undo{
             if(temp instanceof EmptyCell)
                 undoSaved.set(i, new EmptyCell(temp));
             else if(temp instanceof SWITCH)
-                undoSaved.set(i, new SWITCH(temp)); break;
-            }
+                undoSaved.set(i, new SWITCH(temp));
+            else if(temp instanceof AND)
+                undoSaved.set(i, new AND(temp));
+            else if(temp instanceof NAND)
+                undoSaved.set(i, new NAND(temp));
+        }
     }
 }
-
-
-            /*if(currCell instanceof EmptyCell)
-            saveHere.set(i, new EmptyCell(currCell));
-            else if(currCell instanceof SWITCH)
-            saveHere.set(i, new SWITCH(currCell));
-            else if(currCell instanceof AND)
-            saveHere.set(i, new AND(currCell));
-            else if(currCell instanceof NAND)
-            saveHere.set(i, new NAND(currCell));
-            else if(currCell instanceof OR)
-            saveHere.set(i, new OR(currCell));
-            else if(currCell instanceof NOR)
-            saveHere.set(i, new NOR(currCell));
-            else if(currCell instanceof XOR)
-            saveHere.set(i, new XOR(currCell));
-            else if(currCell instanceof NOT)
-            saveHere.set(i, new NOT(currCell));
-            else if(currCell instanceof LAMP)
-            saveHere.set(i, new LAMP(currCell));*/
