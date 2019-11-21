eb95dfdc437098d1f577aada6638a52dd6b5f743
ylygplay@gmail.com <ylygplay@gmail.com>
2019-11-20 20:35:40 -0800

    Added for loop and first two if statements to code

--- LogiSim/app/src/main/java/com/example/logisim/Undo.java
+++ LogiSim/app/src/main/java/com/example/logisim/Undo.java
@@ -3,10 +3,39 @@ import java.util.*;
 
 public class Undo{
     Stack<Cell> save;
-    ArrayList<Cell> temp;
+    Vector<Cell> undoSaved;
     void Undo(Vector<Cell> saveThis){
-        temp = new ArrayList<>();
+        undoSaved = new Vector<>();
         for(int i=0; i<saveThis.size(); i++)
-            temp.add(null);
+            undoSaved.add(null);
+    }
+    void saveUndo(Vector<Cell> saveThis){
+        for(int i=0; i<=saveThis.size();i++){
+            Cell temp=saveThis.get(i);
+            if(temp instanceof EmptyCell)
+                undoSaved.set(i, new EmptyCell(temp));
+            else if(temp instanceof SWITCH)
+                undoSaved.set(i, new SWITCH(temp)); break;
+            }
     }
 }
+
+
+            /*if(currCell instanceof EmptyCell)
+            saveHere.set(i, new EmptyCell(currCell));
+            else if(currCell instanceof SWITCH)
+            saveHere.set(i, new SWITCH(currCell));
+            else if(currCell instanceof AND)
+            saveHere.set(i, new AND(currCell));
+            else if(currCell instanceof NAND)
+            saveHere.set(i, new NAND(currCell));
+            else if(currCell instanceof OR)
+            saveHere.set(i, new OR(currCell));
+            else if(currCell instanceof NOR)
+            saveHere.set(i, new NOR(currCell));
+            else if(currCell instanceof XOR)
+            saveHere.set(i, new XOR(currCell));
+            else if(currCell instanceof NOT)
+            saveHere.set(i, new NOT(currCell));
+            else if(currCell instanceof LAMP)
+            saveHere.set(i, new LAMP(currCell));*/
