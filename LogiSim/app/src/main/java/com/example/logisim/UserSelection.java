2e407a368e1dff47a1f93826e88982c6b894739a
RJCunanan <rjlugtuc@gmail.com>
2019-11-28 11:53:46 -0800

    Fixed bug so that user can actually tap on and select the undo and redo button.

--- LogiSim/app/src/main/java/com/example/logisim/UserSelection.java
+++ LogiSim/app/src/main/java/com/example/logisim/UserSelection.java
@@ -97,7 +97,7 @@ public class UserSelection {
             // Cycle through each button to determine which was selected by the user
             for (int i = 0; i < grid.getButtonList().size(); i++) {
                 if (touchPosition.x == grid.getButtonList().get(i).getButtonXCoordinate()) {
-                    if (touchPosition.x <= LAMP_BUTTON_POSITION) {
+                    if (touchPosition.x <= REDO_BUTTON_POSITION) {
                         // If the x-coordinate of the user's tap matches the x-coordinate of this
                         // button, and the selected button is not a blank button, mark this button
                         // as selected/tapped by the user
