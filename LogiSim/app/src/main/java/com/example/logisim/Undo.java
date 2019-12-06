91dfd7101c5f9cd75dee60172b64981698b60f8c
ylygplay@gmail.com <ylygplay@gmail.com>
2019-12-06 11:42:48 -0800

    changed the save logic

--- LogiSim/app/src/main/java/com/example/logisim/Undo.java
+++ LogiSim/app/src/main/java/com/example/logisim/Undo.java
@@ -4,60 +4,28 @@ import java.util.*;
 public class Undo{
     private Vector<Cell> undoSaved;
     private Stack<Vector<Cell>> save;
+    Save savePlease;
     private Grid grid;
     Undo(Grid gridSave, Vector<Cell> initialArray){
         grid= gridSave;
         save= new Stack<>();
+        savePlease= new Save();
         undoSaved = new Vector<>();
         Cell temp;
-        for(int i=0; i<initialArray.size();i++) {
+        for(int i=0; i<initialArray.size(); i++) {
             temp = initialArray.get(i);
             undoSaved.add(new EmptyCell(temp));
         }
     }
     void saveUndo(Vector<Cell> saveThis){
-        Cell temp;
-        for(int i=0; i<saveThis.size();i++){
-            temp=saveThis.get(i);
-            if(temp instanceof EmptyCell)
-                undoSaved.set(i, new EmptyCell(temp));
-            else if(temp instanceof SWITCH)
-                undoSaved.set(i, new SWITCH(temp));
-            else if(temp instanceof AND)
-                undoSaved.set(i, new AND(temp));
-            else if(temp instanceof NAND)
-                undoSaved.set(i, new NAND(temp));
-            else if(temp instanceof OR)
-                undoSaved.set(i, new OR(temp));
-            else if(temp instanceof NOR)
-                undoSaved.set(i, new NOR(temp));
-            else if(temp instanceof XOR)
-                undoSaved.set(i, new XOR(temp));
-            else if(temp instanceof NOT)
-                undoSaved.set(i, new NOT(temp));
-            else if(temp instanceof LAMP)
-                undoSaved.set(i, new LAMP(temp));
-        }
-        for(int i=0; i<saveThis.size();i++) {
-            temp=saveThis.get(i);
-            if(!(temp instanceof EmptyCell)) {
-                if(temp.getCellA() != null) {
-                    int currCellTailAPos = grid.getCellN(temp.getCellA().cellPosition);
-                    undoSaved.get(i).setCellA(undoSaved.get(currCellTailAPos));
-                }
-                if(temp.getCellB() != null) {
-                    int currCellTailBPos = grid.getCellN(temp.getCellB().cellPosition);
-                    undoSaved.get(i).setCellB(undoSaved.get(currCellTailBPos));
-                }
-                for(int k = 0; k < temp.head.size(); k++) {
-                    int currCellHeadPos = grid.getCellN(temp.head.get(k).cellPosition);
-                    undoSaved.get(i).head.set(k, undoSaved.get(currCellHeadPos));
-                }
-            }
-        }
+        savePlease.saveList(saveThis, undoSaved, grid);
         save.push(undoSaved);
     }
     Vector<Cell> returnUndo() {
         return save.pop();
     }
+    boolean isEmpty(){
+        return save.isEmpty();
+    }
+
 }
