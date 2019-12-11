e394be26dea35e6bb93e2028704552ae190d5500
ylygplay@gmail.com <ylygplay@gmail.com>
2019-12-10 20:08:06 -0800

    changed the undoSave vector into a local variable

--- LogiSim/app/src/main/java/com/example/logisim/Undo.java
+++ LogiSim/app/src/main/java/com/example/logisim/Undo.java
@@ -2,22 +2,20 @@ package com.example.logisim;
 import java.util.*;
 
 class Undo{
-    private Vector<Cell> undoSaved;
     private Stack<Vector<Cell>> save;
     private Save savePlease;
     private Grid grid;
-    Undo(Grid gridSave, Vector<Cell> initialArray){
+    Undo(Grid gridSave){
         grid= gridSave;
         save= new Stack<>();
         savePlease= new Save();
-        undoSaved = new Vector<>();
-        Cell temp;
-        for(int i=0; i<initialArray.size(); i++) {
-            temp = initialArray.get(i);
-            undoSaved.add(new EmptyCell(temp));
-        }
     }
     void saveUndo(Vector<Cell> saveThis){
+        Vector<Cell> undoSaved = new Vector<>();
+        for(int i=0; i<saveThis.size(); i++) {
+            Cell temp = saveThis.get(i);
+            undoSaved.add(new EmptyCell(temp));
+        }
         savePlease.saveList(saveThis, undoSaved, grid);
         save.push(undoSaved);
     }
